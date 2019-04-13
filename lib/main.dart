2c7e555f1e3a9ad97cb48c64b041f09e7b04fba0
Elson Jose <elsonjoseofficial@gmail.com>
2019-04-13 20:49:07 +0530

    First Try

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+
+void main()
+{
+  runApp(MaterialApp(debugShowCheckedModeBanner:false ,
+  home: MainPage()));
+}
+  
+class MainPage extends StatefulWidget {
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  
+  String FinalResult  = "QRCode Result";
+  
+  Future _scanQR() async {
+    
+    try {
+
+      String QRResult = await BarcodeScanner.scan();  
+      setState(() {
+      FinalResult = "RESULT : "+ QRResult;
+
+      });
+
+    } on PlatformException catch(e)
+    {
+      if(e.code ==BarcodeScanner.CameraAccessDenied)
+      {
+        setState(() {
+         FinalResult="Camera Permission Denied.";
+ 
+        });
+      }
+      else{
+        setState(() {
+          FinalResult="Some Error Occured. { $e }";
+ 
+        });
+      }
+    }
+    on FormatException 
+    {
+      setState(() {
+      FinalResult="Scan Incomplete.";
+ 
+      });
+    }
+    catch (e)
+    {
+      setState(() {
+      
+       FinalResult="Some Error Occured. { $e }";
+
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(backgroundColor: Color(0xFFF9A825),title: Center(child: Text("QR Hunter")),),
+     body: new Center(child: Text(FinalResult),), 
+     floatingActionButton: new FloatingActionButton(
+      backgroundColor: Color(0xFFF9A825),
+      child: Icon(Icons.track_changes), 
+      onPressed: ()
+      {
+        _scanQR();
+      }
+     ), 
+     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
